4a0778d038e74296edaad481e6b7b76b6d03c90e
HanDaYeon <75533232+HanDaYeon-coder@users.noreply.github.com>
2023-02-02 17:03:47 +0900

    Delete 보호소에서_중성화한_동물.sql

--- JOIN/보호소에서_중성화한_동물.sql
+++
@@ -1,6 +0,0 @@
-SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
-from ANIMAL_INS as A
-join ANIMAL_OUTS as B on A.ANIMAL_ID = B.ANIMAL_ID
-where A.SEX_UPON_INTAKE like "Intact%" and B.SEX_UPON_OUTCOME like "Spayed%"
-or A.SEX_UPON_INTAKE like "Intact%" and B.SEX_UPON_OUTCOME like "Neutered%"
-order by B.ANIMAL_ID
